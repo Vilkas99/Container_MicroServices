f4df6fd1ebd9ec9098daa8499f042073e6074544
vilkas99 <victorium99@hotmail.com>
2021-11-08 10:45:13 -0600

    Handling microfronts down

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { NavLink, BrowserRouter, Route, Switch } from 'react-router-dom';
-import MicroFrontend from './MicroFrontend';
+import React, { useState } from "react";
+import { NavLink, BrowserRouter, Route, Switch } from "react-router-dom";
+import MicroFrontend from "./MicroFrontend";
 
 //Obtenemos la dirección de nuestro microFront por parte del .env
 //En local, apuntamos al puerto 4000; en producción apuntaríamos a la URL que hostea a la aplicación.
 const { REACT_APP_CREATEREACTAPP_HOST: microFrontHost } = process.env;
 
-
 //Componente que requiere el "History" de la aplicación (Es decir, el registro de la URL en la que nos encontramos)
-const MicroFront = ({ history }) => (
+const MicroFront = ({ history, setError }) => (
   //Llamaos a nuestro componente que posee todas las configuraciones para añadir al microfront
   //Le establecemos su historia, la dirección del host y el nombre del componente.
   <MicroFrontend
     history={history}
     host={microFrontHost}
     name="microfront"
+    setError={setError}
   />
 );
+
+//Componente que reemplaza al microfront cuando este se encuentra inalcanzable
+const DummyComponent = () => {
+  return <h1>Ups, hubo un error</h1>;
+};
 
 //Componente propio del proyecto que funje sólo para exponer como podemos combinar componentes de distintos proyectos.
 const Hogar = () => (
@@ -31,6 +36,12 @@ const Hogar = () => (
 
 //Aplicaciónb principal
 const App = (props) => {
+  const [error, setError] = useState(false);
+
+  const updateError = (value) => {
+    setError(value);
+  };
+
   return (
     <BrowserRouter>
       <h1>Este es un ejemplo de la arquitectura de "MicroFrontends"</h1>
@@ -47,10 +58,15 @@ const App = (props) => {
           <NavLink to="/microFront">Micro Frontend</NavLink>
         </li>
       </ul>
-
       <Switch>
         <Route path="/hogar" component={Hogar} />
-        <Route path="/microFront" render={() => <MicroFront />} />
+        <Route
+          path="/microFront"
+          render={() =>
+            //Si tenemos un error (En este caso correspondiente el microFront en cuestión) entonces renderizaremos al "DummyComponent", de lo contrario, renderizamos al microfront
+            error ? <DummyComponent /> : <MicroFront setError={updateError} />
+          }
+        />
       </Switch>
     </BrowserRouter>
   );
--- src/MicroFrontend.js
+++ src/MicroFrontend.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 //Componente funcional que se encarga de vincular los scripts del proyecto de Micro con el Contenedor.
-const MicroFrontend = ({ name, host, history }) => {
+const MicroFrontend = ({ name, host, history, setError }) => {
   //Hook que se ejecuta cada vez que hay un cambio en el nombre, host o history
   useEffect(() => {
     //Generamos el id del script (El cual debe coincidir con el del microFrontend)
@@ -19,21 +19,33 @@ const MicroFrontend = ({ name, host, history }) => {
     }
 
     //Si no, entonces...
-    //Realizamos un fetch al manifiesto json del host que se nos proporcionó a través del parámetro. 
+    //Realizamos un fetch al manifiesto json del host que se nos proporcionó a través del parámetro.
     fetch(`${host}/asset-manifest.json`)
       //Lo transformamos a json
-      .then(res => res.json())
+      .then((res) => {
+        //Si la respuesta del servidor es "Ok" entonces regresaremos sus valores como un JSON para poder ser utilizados.
+        if (res.ok) {
+          return res.json;
+          //De lo contrario, regresaremos un error con la finalidad de realizar un "catch"
+        } else {
+          throw new Error("No se logró comunicar con el microfront");
+        }
+      })
       //Accedemos al manifiesto
-      .then(manifest => {
+      .then((manifest) => {
         //Obtenemos todos aquellos elementos que tenga la key de "files" y los almacenamos en "promises"
-        const promises = Object.keys(manifest['files'])
-          .filter(key => key.endsWith('.js')) //Los filtramos para solo seleccionar los elementos que tengan extensión 'js'
-          .reduce((sum, key) => { //Los reducimos a su "sum" (Contenido) y su key
-            sum.push( //A cada contenido le añadimos...
-              new Promise(resolve => { //Una nueva promesa
-                const path = `${host}${manifest['files'][key]}`; //Creamos una variable que se llama path
-                const script = document.createElement('script'); //Creamos un script
-                if (key === 'main.js') { //Si la llave es "main.js" significa que habremos encontrado el script de código principal
+        const promises = Object.keys(manifest["files"])
+          .filter((key) => key.endsWith(".js")) //Los filtramos para solo seleccionar los elementos que tengan extensión 'js'
+          .reduce((sum, key) => {
+            //Los reducimos a su "sum" (Contenido) y su key
+            sum.push(
+              //A cada contenido le añadimos...
+              new Promise((resolve) => {
+                //Una nueva promesa
+                const path = `${host}${manifest["files"][key]}`; //Creamos una variable que se llama path
+                const script = document.createElement("script"); //Creamos un script
+                if (key === "main.js") {
+                  //Si la llave es "main.js" significa que habremos encontrado el script de código principal
                   script.id = scriptId; //Establecemos que su ID será el "scriptID" que creamos anteriormente
                 }
                 script.onload = () => {
@@ -45,9 +57,17 @@ const MicroFrontend = ({ name, host, history }) => {
             );
             return sum;
           }, []);
-        Promise.allSettled(promises).then(() => { //Una vez que todas las promesas se hayan cumplido...
-          renderMicroFrontend(); //Renderizamos 
+        Promise.allSettled(promises).then(() => {
+          //Una vez que todas las promesas se hayan cumplido...
+          renderMicroFrontend(); //Renderizamos
         });
+      })
+      //Si hubo algún error, lo atraparemos aquí
+      .catch((error) => {
+        //Actualizamos el estado del "error" a verdadero (De esta forma, le decimos al parent que este microfront no puede renderizarse)
+        setError(true);
+        //Imprimimos el error
+        console.log(error);
       });
 
     return () => {
